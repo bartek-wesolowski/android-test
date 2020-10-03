5295e16c829247a16dd091c9ab102a2b9056f612
Bartosz Wesolowski <bartosz.wesolowski@allegro.pl>
2020-10-03 21:13:15 +0200

    Add separate backstacks for different tabs

--- app/src/main/java/com/hr/test/MainActivity.kt
+++ app/src/main/java/com/hr/test/MainActivity.kt
@@ -2,16 +2,23 @@ package com.hr.test
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import life.shank.android.AutoScoped
 
 class MainActivity : AppCompatActivity(), AutoScoped {
 
+    lateinit var controller: LiveData<NavController>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<BottomNavigationView>(R.id.bottom_navigation).setupWithNavController(findNavController(R.id.nav_host_fragment))
+        controller = findViewById<BottomNavigationView>(R.id.bottom_navigation).setupWithNavController(
+            navGraphIds = listOf(R.navigation.list, R.navigation.grid),
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_container,
+            intent = intent
+        )
     }
 }
